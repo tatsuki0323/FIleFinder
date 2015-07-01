bc4f566ebaa7978f05de72f2476ab485a698c107
niwatorihada <sugiharakouichirou@jazzs-MacBook-Air-2.local>
2015-07-02 01:57:56 +0900

    コマンドラインオプション解析処理を作成しました
    コマンドラインオプションを解析する処理を追加しました。
    CmdLineExceptionとCmdLineParserをimportしました。

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -1,10 +1,22 @@
 package com.github.finder;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 
 public class Main{
-    public Main(String[] arguments){
-    }
-
-    public static void main(String[] args){
-        new Main(args);
-    }
+	public Main(String[] arguments){
+	}
+	
+	public static void main(String[] args){
+		new Main(args);
+	}
+	
+	private Args parseArguments(String[] arguments){
+		Args args = new Args();
+		try {
+			CmdLineParser parser = new CmdLineParser(args);
+			parser.parseArgument(arguments);
+		} catch (CmdLineException e) {
+		}
+		return args;
+	}
 }
